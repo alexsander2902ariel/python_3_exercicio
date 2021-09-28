c9c1214dbef7115b2b79f441e24a79536b2769d4
alexsander2902ariel <alexsander2902ariel@gmail.com>
2021-09-27 22:24:39 -0300

    hotfix

--- test/main.py
+++ test/main.py
@@ -1,11 +1,11 @@
 def sum_numbers_sequence(vet):
     cont=0
     for i in range(len(vet)):
-       cont= cont+vet[i]
-    return(cont)
+       cont= cont+(vet[i]*10)
+    return(cont/10)
 
 def div_numbers_sequence(x,y):
     cont=0
     if (y!= 0):
-        cont=x/y
+        cont=(x*10)/(y*10)
     return(cont)
--- test/test_pytest.py
+++ test/test_pytest.py
@@ -9,3 +9,8 @@ def test_check_div_1():
     assert div_numbers_sequence(10,5) == 2
 def test_check_div_2():
     assert div_numbers_sequence(.5,.1) == 5
+
+def test_check_sum_3():
+        assert sum_numbers_sequence([.1,.2]) == .3 
+def test_check_div_3():
+    assert div_numbers_sequence(.3,.1) == 3
